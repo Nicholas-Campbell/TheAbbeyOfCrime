3fa25fddec78dd9f039c071c0ee0674a3b79cc34
Nicholas Campbell <35669586+Nicholas-Campbell@users.noreply.github.com>
2021-04-14 15:55:30 +0100

    Updated English translation of ending text
    The previous version of this file mistakenly contained the original Spanish text

--- asm/ending_text_en.asm
+++ asm/ending_text_en.asm
@@ -1,4 +1,4 @@
-; The Abbey of Crime - ending text
+; La Abadia del Crimen - ending text
 ;
 ; Byte 13 (&0D) indicates the end of a line
 ; Byte 26 (&1A) indicates the end of the text
@@ -6,123 +6,121 @@
 
 org &6e58
 
-db " Desfigurado por la",13
-db "angustia, por el aco-",13
-db "so del veneno que ya",13
-db "serpenteaba abundante",13
-db "por sus venas, el",13
-db "otrora venerable ros-",13
-db "tro del anciano se",13
-db "veia repulsivo y gro-",13
-db "tesco.",13
-db " Habriamos podido",13
-db "atraparlo con calma,",13
-db "pero nos precipitamos",13
-db "con vehemencia sobre",13
-db "el. Logro zafarse y",13
-db "apreto el libro contra",13
-db "su pecho para defen-",13
-db "derlo. Yo lo tenia co-",13
-db "gido con la mano iz-",13
-db "quierda, mientras con",13
-db "la derecha trataba de",13
-db "mantener en alto la",13
-db "lampara. Pero roce",13
-db "su rostro con la lla-",13
-db "ma y emitio un so-",13
-db "nido ahogado, casi un",13
-db "rugido, dejando caer",13
-db "trozos de folio por",13
-db "la boca. Su mano",13
-db "derecha solto el li-",13
-db "bro, busco la lampa-",13
-db "ra y, de un golpe, me",13
-db "la arranco lanzandola",13
-db "hacia adelante...",13
-db " Se derramo el acei-",13
-db "te, y en seguida el",13
-db "fuego prendio en el",13
-db "pergamino que ardio",13
-db "como un haz de hor-",13
-db "nija reseca. Todo su-",13
-db "cedio en pocos instan-",13
-db "tes; una llamarada se",13
-db "elevo desde los libros,",13
-db "como si aquellas pa-",13
-db "ginas milenarias lle-",13
-db "varan siglos esperan-",13
-db "do quemarse y goza-",13
-db "ran al satisfacer de",13
-db "pronto una sed inme-",13
-db "morial de ecpirosis.",13
-db " La abadia ardio du-",13
-db "rante tres dias y tres",13
-db "noches y de nada va-",13
-db "lieron los ultimos es-",13
-db "fuerzos.  Al tercer",13
-db "dia, curados los heri-",13
-db "dos, enterrados los",13
-db "cadaveres que habian",13
-db "quedado fuera de los",13
-db "edificios, los monjes",13
-db "y el resto de los po-",13
-db "bladores de la abadia",13
-db "recogieron sus perte-",13
-db "nencias y abandonaron",13
-db "la meseta, que aun",13
-db "humeaba, como un lu-",13
-db "gar maldito.",13
-db " Guillermo y yo nos",13
-db "alejamos de aquel pa-",13
-db "raje en dos cabalga-",13
-db "duras que encontramos",13
-db "por el bosque. Cuando",13
-db "llegamos a  Munich",13
-db "tuve que separarme,",13
-db "no sin derramar",13
-db "abundantes lagrimas,",13
-db "de mi buen maestro.",13
-db "Despues de darme",13
-db "muchos buenos con-",13
-db "sejos para mis fu-",13
-db "turos estudios, me",13
-db "estrecho entre sus",13
-db "brazos, con la ter-",13
-db "nura de un padre, y",13
-db "me dijo adios. Jamas",13
-db "volvi a verlo.",13
-db " Ahora, ya en el um-",13
-db "bral de la muerte,",13
-db "cuanto mas releo la",13
-db "historia que de ello ha",13
-db "resultado, menos se",13
-db "si esta contiene o no",13
-db "una trama distinguible",13
-db "de la mera sucesion",13
-db "natural de los aconte-",13
-db "cimientos y de los",13
-db "momentos que los re-",13
-db "lacionan entre si. Y",13
-db "es duro para este",13
-db "viejo monje no saber",13
-db "si la letra que ha es-",13
-db "crito contiene o no",13
-db "algun sentido oculto,",13
-db "ni si contiene mas de",13
-db "uno, o muchos, o nin-",13
-db "guno.",13
-db " Pero quizas esta",13
-db "incapacidad sea pro-",13
-db "ducto de la sombra",13
-db "que la gran tiniebla",13
-db "que se aproxima, pro-",13
-db "yecta  sobre  este",13
-db "mundo ya viejo.",13
-db " Hace frio en el",13
-db "scriptorium, me duele",13
-db "el pulgar. Dejo este",13
-db "texto, no se para",13
-db "quien, este texto, que",13
-db "ya no se de que ha-",13
-db "bla.",13
+db 13
+db " Disfigured by anxie-",13
+db "ty, by the menace of",13
+db "the poison now flowing",13
+db "abundantly through his",13
+db "veins, the venerable",13
+db "figure of the old man",13
+db "now seemed disgusting",13
+db "and grotesque.",13
+db 13
+db " We could have taken",13
+db "him calmly, but we",13
+db "fell on him with vio-",13
+db "lence; he writhed,",13
+db "clasped his hands on",13
+db "his chest to defend",13
+db "the volume; I grasped",13
+db "him with my left hand",13
+db "while with my right",13
+db "I tried to hold the",13
+db "lamp high, but I gra-",13
+db "zed his face with the",13
+db "flame, he sensed the",13
+db "heat, let out a muf-",13
+db "fled cry, almost a",13
+db "roar, as bits of paper",13
+db "spilled from his mouth,",13
+db "and his right hand let",13
+db "go of the volume, dar-",13
+db "ted toward the lamp,",13
+db "and abruptly tore it",13
+db "from me, flinging it",13
+db "away...",13
+db 13
+db " The oil spilled out,",13
+db "the fire immediately",13
+db "seized  a  fragile",13
+db "parchment, which bla-",13
+db "zed up like a bundle",13
+db "of dry twigs. Every-",13
+db "thing happened in a",13
+db "few moments, as if",13
+db "for centuries those",13
+db "ancient pages had been",13
+db "yearning for arson and",13
+db "were rejoicing in the",13
+db "sudden satisfaction",13
+db "of an immemorial",13
+db "thirst for ecpyrosis.",13
+db 13
+db " The Abbey burned",13
+db "for three days and",13
+db "three nights, and the",13
+db "last efforts were of",13
+db "no avail.",13
+db 13
+db " On the third day,",13
+db "when the wounded had",13
+db "been treated and the",13
+db "corpses  had  been",13
+db "buried, the monks and",13
+db "all the others collec-",13
+db "ted their belongings",13
+db "and abandoned the",13
+db "still-smoking abbey,",13
+db "as a place accursed.",13
+db 13
+db " William and I left",13
+db "those parts on two",13
+db "horses  we  found",13
+db "astray in the woods.",13
+db "When we came to",13
+db "Munich, I had to take",13
+db "leave of my good",13
+db "master, amid many",13
+db "tears. He gave me",13
+db "much good advice about",13
+db "my future studies,",13
+db "embraced me with a",13
+db "father's tenderness",13
+db "and dismissed me. I",13
+db "never saw him again.",13
+db 13
+db " The more I repeat",13
+db "to myself the story,",13
+db "the less I manage to",13
+db "understand whether in",13
+db "it there is a design",13
+db "that goes beyond the",13
+db "natural sequence of",13
+db "the events and the",13
+db "times that connect",13
+db "them. And it is a",13
+db "hard thing for this old",13
+db "monk, on the threshold",13
+db "of death, not to know",13
+db "whether the letter he",13
+db "has written contains",13
+db "some hidden meaning,",13
+db "or more than one, or",13
+db "many, or none at all.",13
+db 13
+db " But this inability",13
+db "of mine to see is",13
+db "perhaps the effect of",13
+db "the shadow that the",13
+db "great darkness, as",13
+db "it approaches, is",13
+db "casting on the aged",13
+db "world.",13
+db " It is cold in the",13
+db "scriptorium, my thumb",13
+db "aches. I leave this",13
+db "manuscript, I do not",13
+db "know for whom; I no",13
+db "longer know what it",13
+db "is about.",13
 db 26
